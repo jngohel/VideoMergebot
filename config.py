ae37092065476dfdd150efd282b5abf3b2a6332e
jngohel <136894773+jngohel@users.noreply.github.com>
2024-03-14 14:04:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
+    API_HASH = os.environ.get("b2ec3ab8ed156e7a6782f3d7d1acbaf6")
+    BOT_TOKEN = os.environ.get("7028334632:AAEXTOfWu3E9eXMrQbHzPKdtWroPRR3jelk")
+    TELEGRAM_API = os.environ["24998279"]
     OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    OWNER_USERNAME = os.environ.get("JNGohell")
+    PASSWORD = os.environ.get("Abhi96")
+    DATABASE_URL = os.environ.get("mongodb+srv://Jng:jng@cluster0.vvrnobg.mongodb.net/?retryWrites=true&w=majority")
+    LOGCHANNEL = os.environ.get("-1001849681109")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
